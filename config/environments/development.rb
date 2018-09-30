d5a13200581fef1263fa6e8117cea4f04c9f47ba
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-09-30 02:03:21 -0700

    Added and configured Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -45,7 +47,7 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
