94da5f3e193818b02f4cb07b3f10a564a87e0eec
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-10-07 20:15:00 -0700

    Added keyword spec

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Keyword, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with a club" do
+    let!(:club) {create(:club)}
+    
+    it "creates a keyword for the club" do
+      create(:keyword, label: "finance", club: club)
+      expect(club.keywords.count).to eq(1)
+      expect(club.keywords.first.label).to eq("finance")
+    end
+    
+    it "creates multiple keywords for the club" do
+      create(:keyword, label: "finance", club: club)
+      create(:keyword, label: "investments", club: club)
+      expect(club.keywords.count).to eq(2)
+    end
+    
+    it 'does not create duplicate keywords' do
+      create(:keyword, label: "finance", club: club)
+      expect{create(:keyword, label: "finance", club: club)}.to raise_error(ActiveRecord::RecordNotUnique)
+      expect(club.keywords.count).to eq(1)
+    end
+  end
 end
