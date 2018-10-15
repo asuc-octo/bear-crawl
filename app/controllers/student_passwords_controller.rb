e65ae1ecd493780ab6f5e88189aa4fab6820446a
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-10-14 20:52:21 -0700

    Added student passwords controller

---
+++ app/controllers/student_passwords_controller.rb
@@ -0,0 +1,53 @@
+class StudentPasswordsController < Devise::PasswordsController
+  before_action :verify_student_reset, only: :edit
+
+  def create
+    @student = Student.send_reset_password_instructions(request_params)
+    if successfully_sent?(@student)
+      sign_out("student")
+      cookies.delete :student_id
+      cookies.delete :is_signed_in
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    @student = Student.new
+    set_minimum_password_length
+    @student.reset_password_token = params[:reset_password_token]
+  end
+
+  def update
+    @student = Student.reset_password_by_token(new_password_params)
+
+    if @student.errors.empty?
+      sign_in(:student, @student)
+      cookies.signed.permanent[:student_id] = @student.id
+      cookies.signed.permanent[:is_signed_in] = "student"
+      redirect_to student_profiles_path(@student)
+    else
+      set_minimum_password_length
+      render json: {
+        "status": "error",
+        "message": "errors"
+        }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def request_params
+    params.require(:student).permit(:email)
+  end
+
+  def new_password_params
+    params.require(:student).permit(:reset_password_token, :password, :password_confirmation)
+  end
+
+  def verify_student_reset
+    if !Student.with_reset_password_token(params[:reset_password_token]).present?
+      redirect_to root_path
+    end
+  end
+end
