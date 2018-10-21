7442b22d861829c5c086b07c32f8f59d80aaefdd
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-10-20 18:30:38 -0700

    Renamed routes to more specific ones, generated views

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,33 @@ Rails.application.routes.draw do
   devise_for :clubs, 
              path: '',
              path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
+               sign_in: 'club/login',
+               sign_out: 'club/logout',
+               registration: 'club/signup',
+               password: 'club/password/',
+               confirmation: 'club/confirmation'
              },
              controllers: {
                sessions: 'club_sessions',
                registrations: 'club_registrations',
-               confirmations: 'club_confirmations'
+               confirmations: 'club_confirmations',
+               passwords: 'club_passwords'
              }
 
   devise_for :students,
              path: '',
              path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
+               sign_in: 'student/login',
+               sign_out: 'student/logout',
+               registration: 'student/signup',
+               password: 'student/password/',
+               confirmation: 'student/confirmation'
              },
              controllers: {
                sessions: 'student_sessions',
                registrations: 'student_registrations',
-               confirmations: 'student_confirmations'
+               confirmations: 'student_confirmations',
+               passwords: 'student_passwords'
              }
   
   
